601640a1f92962e8292fc6d88cc310e57330df41
zinntikumugai <uesitananame55@gmail.com>
2018-06-03 00:19:25 +0900

    develop js

--- loader.js
+++ loader.js
@@ -1,3 +1,13 @@
-$.getJSON('./data/infomation.json', function(json) {
-    console.log(json);
+console.log("laoder");
+$.ajax({
+    type: 'GET',
+    url: "./data/infomation.json",
+    dataType: "json",
+    success: function(json) {
+        console.log(json);
+    },
+    error: function(err) {
+        console.log(err);
+    }
 });
+console.log("end");
